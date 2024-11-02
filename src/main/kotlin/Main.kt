37ce9feb283de10a5b3476cd3bc463955783f1dc
miroks404 <themiroks@gmail.com>
2024-11-02 12:31:07 +0200

    PGB-01 realize password generator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,43 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main() {
+    val passwordLength: Int
+    try {
+        print("Введите длину пароля (минимум 6 символов, максимум 200 символов): ")
+        passwordLength = getLength()
+    } catch (e: Error) {
+        println("Минимум 6 символов!")
+        return main()
+    } catch (e: ArithmeticException) {
+        println("Максимум 200 символов!")
+        return main()
     }
+    catch (e: Exception) {
+        println("Следует вводить только числа!")
+        return main()
+    }
+
+    val password = getPassword(passwordLength)
+
+    println(password)
+}
+
+fun getLength() : Int {
+    val length = readln().toInt()
+    if (length < 6) throw Error()
+    if (length > 200) throw ArithmeticException()
+    return length
+}
+
+fun getPassword(length: Int) : String {
+    val allChars: List<Char> = ('A'..'Z') + ('a'..'z') + ('0'..'9') + ('!'..'~')
+
+    var password = ""
+
+    for (i in 1..length) {
+        password += allChars.random()
+    }
+
+    return password
 }
