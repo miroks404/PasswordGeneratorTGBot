2e2b252d13903b5517b2c272c7795ed6ee60d36f
miroks404 <themiroks@gmail.com>
2024-11-07 15:46:05 +0200

    PGB-01 corrections and add number of generated passwords

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,32 +12,67 @@ fun main() {
     } catch (e: ArithmeticException) {
         println("Максимум 200 символов!")
         return main()
-    }
-    catch (e: Exception) {
+    } catch (e: Exception) {
         println("Следует вводить только числа!")
         return main()
     }
 
-    val password = getPassword(passwordLength)
+    var numberOfGeneratedPasswords: Int
+
+    while (true) {
+        try {
+            print("Сколько паролей нужно сгенерировать? (минимум 1, максимум 100): ")
+            numberOfGeneratedPasswords = getNumberOfGeneratedPasswords()
+            break
+        } catch (e: Exception) {
+            println("Следует вводить только целые числа!")
+        } catch (e: Error) {
+            println("Меньше 1 пароля не генерируется!")
+        } catch (e: ArithmeticException) {
+            println("Больше 100 паролей не генерируется!")
+        }
+    }
+
+    val password = getPassword(passwordLength, numberOfGeneratedPasswords)
 
     println(password)
 }
 
-fun getLength() : Int {
+fun getLength(): Int {
     val length = readln().toInt()
-    if (length < 6) throw Error()
-    if (length > 200) throw ArithmeticException()
+    if (length < MINIMUM_LENGTH_OF_PASSWORD) throw Error()
+    if (length > MAXIMUM_LENGTH_OF_PASSWORD) throw ArithmeticException()
     return length
 }
 
-fun getPassword(length: Int) : String {
+fun getNumberOfGeneratedPasswords(): Int {
+    val numberOfGeneratedPasswords = readln().toInt()
+    if (numberOfGeneratedPasswords < MINIMUM_COUNT_OF_GENERATED_PASSWORDS) throw Error()
+    if (numberOfGeneratedPasswords > MAXIMUM_COUNT_OF_GENERATED_PASSWORDS) throw ArithmeticException()
+    return numberOfGeneratedPasswords
+}
+
+fun getPassword(length: Int, numberOfGeneratedPasswords: Int): String {
     val allChars: List<Char> = ('A'..'Z') + ('a'..'z') + ('0'..'9') + ('!'..'~')
 
-    var password = ""
+    val listOfPasswords: MutableList<String> = mutableListOf()
 
-    for (i in 1..length) {
-        password += allChars.random()
+    for (count in 1..numberOfGeneratedPasswords) {
+
+        var password = ""
+
+        for (i in 1..length) {
+            password += allChars.random()
+        }
+
+        listOfPasswords.add(password)
     }
 
-    return password
+    return listOfPasswords.joinToString("\n")
 }
+
+const val MINIMUM_LENGTH_OF_PASSWORD = 6
+const val MAXIMUM_LENGTH_OF_PASSWORD = 200
+
+const val MINIMUM_COUNT_OF_GENERATED_PASSWORDS = 1
+const val MAXIMUM_COUNT_OF_GENERATED_PASSWORDS = 100
