ce33acc807940efe69c3574d3ed4b6e56a19adb4
miroks404 <themiroks@gmail.com>
2024-11-08 15:53:58 +0200

    PGB-02 put in tg bot

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,12 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://jitpack.io")
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation("org.telegram:telegrambots:6.0.1") // Telegram Bot API
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.17.1")
 }
 
 tasks.test {
